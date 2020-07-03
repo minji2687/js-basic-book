0a9f204ca866975031bd0b142a5d1310ed8f1748
Jo Minji <mingi2687@gamil.com>
2020-07-03 15:26:25 +0900

    여행 준비물 한번 더  실습 테스트

--- 07/js/diy-itemList.js
+++ 07/js/diy-itemList.js
@@ -1,44 +1,47 @@
-		var itemList = [];
-		var addBtn = document.querySelector('#add');
-		addBtn.addEventListener("click", addList);
-		
-		getItems();
+var itemList = [];
 
-		function getItems() {
-			var storedData = localStorage.getItem("storageList");
-			if(storedData != null) itemList = JSON.parse(storedData);
-			showList();
-		}		
-				
-		function addList() {
-			var item = document.querySelector('#item').value;
-			if(item != null) {
-				itemList.push(item);
-				document.querySelector('#item').value = "";
-				document.querySelector('#item').focus();
-			}			
-			localStorage.setItem("storageList", JSON.stringify(itemList));
-			showList();
-		}	
+var button = document.querySelector("#add");
 
-		function showList() {						
-			var list = "<ul>";
-			for (var i=0; i<itemList.length; i++) {
-				list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">X</span></li>";
-			}
-			list += "</ul>";
+button.addEventListener("click", addList);
 
-			document.querySelector('#itemList').innerHTML = list;
+function addList() {
+  var item = document.querySelector("#item");
+  if (item.value !== NaN) {
+    itemList.push(item.value);
+    item.value = "";
+  }
 
-			var remove = document.getElementsByClassName('close');
-			for (var i=0; i<remove.length; i++) {
-				remove[i].addEventListener("click", removeList);
-			}
-		}
+  showList();
+  item.value = "";
+}
 
-		function removeList() {			
-			var id = this.getAttribute("id");	
-			itemList.splice(id, 1);
-			localStorage.setItem("storageList", JSON.stringify(itemList));
-			showList();
-		}
+function showList() {
+  var list = "<ul>";
+  for (var i = 0; i < itemList.length; i++) {
+    list +=
+      "<li>" +
+      itemList[i] +
+      "<span class='close' id=" +
+      i +
+      ">X</span>" +
+      "</li>";
+  }
+  list += "</ul>";
+
+  document.querySelector("#itemList").innerHTML = list;
+  var remove = document.querySelectorAll(".close");
+  // nodeList를 반환하기때문에 이벤트리스너를 걸 때 for문을 이용해서
+  //각각의 Element에 이벤트를 걸어줘야함.
+
+  for (var i = 0; i < itemList.length; i++) {
+    remove[i].addEventListener("click", removeList);
+  }
+}
+
+function removeList() {
+  console.log(this);
+  var id = this.getAttribute("id");
+  itemList.splice(id, 1);
+
+  showList();
+}
--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -1,11 +1,16 @@
-		var numbers = [2, 4, 6, 8, 10];
-		showArray(numbers);
+var numbers = [2, 4, 6, 8, 10];
+showArray(numbers);
 
-		function showArray(arr) {
-			var str = "<table><tr>";
-			for (var i=0; i<arr.length; i++) {
-				str += "<td>" + arr[i] + "</td>";
-			}
-			str += "</tr></table>";
-			document.write(str);
-		}
+function showArray(arr) {
+  var str = "<table><tr>";
+  for (var i = 0; i < arr.length; i++) {
+    str += "<td>" + arr[i] + "</td>";
+  }
+  var lastNum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    lastNum += arr[i];
+  }
+  str += "<td>" + lastNum + "</td>";
+  str += "</tr></table>";
+  document.write(str);
+}
